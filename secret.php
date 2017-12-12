e0e4e78af7b0bf717d298402a965f6170f738021
Nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-12 16:12:58 +0100

    Mise en place du mot de passe kangourou

--- secret.php
+++ secret.php
@@ -5,5 +5,11 @@
         <title>Mot de passe pour les infos du serveur central de TF1 </title>
     </head>
     <body>  
+        <?php
+    if (isset($_POST['password']) AND $_POST['password'] ==  "kangourou") // Mot de passe correct
+    {
+
+// nous mettons en places le code suivant :
+    }
     </body>
 </html>
