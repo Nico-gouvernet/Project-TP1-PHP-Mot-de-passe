7b81f3841f8abf0dc5688b794ef9d5443df885c4
Nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-12 15:59:32 +0100

    Mise en place de la structure de base pour le fichier secret.php

---
+++ secret.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mot de passe pour les infos du serveur central de TF1 </title>
+    </head>
+    <body>  
+    </body>
+</html>
