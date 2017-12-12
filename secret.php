a32d6f7d4d466b3aafa793f586facd8b9ea8bc95
Nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-12 16:19:41 +0100

    message indiquant qu'ils ont trouvé le bon message

--- secret.php
+++ secret.php
@@ -10,6 +10,20 @@
     {
 
 // nous mettons en places le code suivant :
+?>
+<h1>Voila le code :</h1>
+        <p><strong>KK09-365I-C585-JDFM-DSFN-78D1-FS28-SFFV</strong></p>   
+        
+        <p>
+        Cette page est pour le personnel de TF1. Attention N'oubliez pas que les codes d'accès sont changés régulierement.<br />
+        TF1 vous remercie de votre visite.
+        </p>
+        <?php
     }
+    else // Sinon, message d'erreur
+    {
+        echo '<p>Mot de passe invalide</p>';
+    }
+    ?>
     </body>
 </html>
