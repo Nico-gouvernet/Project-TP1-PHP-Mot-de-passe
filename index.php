e21eb787a57bb232d3352d622d8de3789869d8ee
Nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-12 15:47:12 +0100

    Mise en place du formulaire

--- index.php
+++ index.php
@@ -5,5 +5,11 @@
 </head>
 <body>
 <p> Veuillez entrer le mot de passe pour acceder au fichier <strong>secret.php</strong></p>
+<form action="secret.php" method="post">
+            <p>
+            <input type="password" name="password" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
 </body>
 </html>
